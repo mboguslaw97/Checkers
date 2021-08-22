6860a1c68998a209f885bb4282bae3493814d19b
Michael Boguslaw <mboguslaw97@gmail.com>
2021-08-22 02:46:15 -0400

    class based ai implementation

--- ai.py
+++ ai.py
@@ -11,42 +11,49 @@ def get_moves(board):
     return moves
 
 
-def random_move(board):
-    move = get_moves(board)[0]
-    board.move_and_update_turn(*move)
+class RandomAI:
+    def move(board):
+        move = get_moves(board)[0]
+        board.move_and_update_turn(*move)
 
 
-def minimax_move(board):
-    def metric(board):
-        if color == WHITE:
-            c1, c2 = board.white_count, board.red_count
-        else:
-            c1, c2 = board.red_count, board.white_count
-        if c2 == 0:
-            return 20
-        return c1 / c2
+class MinimaxAI:
+    def __init__(self, depth):
+        self.depth = depth
 
-    def recurse(board, depth):
+    def move(self, board):
+        self.color = board.turn
+        _, move = self.minimax(board, self.depth)
+        board.move_and_update_turn(*move)
+
+    def minimax(self, board, depth):
         if depth == 0 or board.gameover:
-            return metric(board), None
+            return self.metric(board), None
 
-        use_max = board.turn == color
+        use_max = board.turn == self.color
         depth2 = depth if board.can_jump() else depth - 1
         best_val = -1 if use_max else 20
         best_move = None
         for move in get_moves(board):
             board2 = deepcopy(board)
             board2.move_and_update_turn(*move)
-            val, _ = recurse(board2, depth2)
-            if depth == 4:
-                print(val, move)
-            # Optimization: undo move instead of creating copy
+            val, _ = self.minimax(board2, depth2)
+            # if depth == 4:
+            #     print(val, move)
+            # Todo: undo move instead of creating copy
             # board.undo_move_and_update_turn(*move)
             if use_max and val > best_val or not use_max and val < best_val:
                 best_val = val
                 best_move = move
         return best_val, best_move
 
-    color = board.turn
-    _, move = recurse(board, 4)
-    board.move_and_update_turn(*move)
+    # Todo: kings should be worth more points
+    # Todo: the closer a piece is to the back rank, the higher the score
+    def metric(self, board):
+        if self.color == WHITE:
+            c1, c2 = board.white_count, board.red_count
+        else:
+            c1, c2 = board.red_count, board.white_count
+        if c2 == 0:
+            return 20
+        return c1 / c2
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from ai import MinimaxAI, RandomAI
 from board import Board
 from constants import FPS, HEIGHT, RED, WHITE, WIDTH
 from util import xy_to_rc
-import ai
 import pygame
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -13,27 +13,27 @@ board = Board()
 running = True
 
 # Set AI to None for human input
-white_ai_move = ai.minimax_move
-# red_ai_move = ai.random_move
-red_ai_move = None
+white_ai = MinimaxAI(2)
+red_ai = MinimaxAI(4)
+# red_ai = None
 
 while running:
     board.draw(screen)
     pygame.display.update()
-    clock.tick(FPS)
+    clock.tick(5)
 
     if not board.gameover:
-        if board.turn == WHITE and white_ai_move is not None:
-            white_ai_move(board)
-        if board.turn == RED and red_ai_move is not None:
-            red_ai_move(board)
+        if board.turn == WHITE and white_ai is not None:
+            white_ai.move(board)
+        elif board.turn == RED and red_ai is not None:
+            red_ai.move(board)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and not board.gameover and \
-                (board.turn == WHITE and white_ai_move is None or board.turn == RED and red_ai_move is None):
+                (board.turn == WHITE and white_ai is None or board.turn == RED and red_ai is None):
             pos = pygame.mouse.get_pos()
             rc = xy_to_rc(pos)
             board.select(rc)
